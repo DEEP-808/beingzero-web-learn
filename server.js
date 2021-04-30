d457db6f2a1c2ad8b137a256b6778df522f5b10a
Deepak Nadiminti <deepaknadiminti12@gmail.com>
2021-04-30 11:09:47 +0530

    added styled resume

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
  
 const app = express();
- 
+
 app.use(express.static('frontend'));
+
 app.get("/", function(req, res){
     res.send("Welcome to My Basic Site");
 })
  
 app.get("/resume", function(req, res){
-    let path=__dirname+"frontend/html/page.html";
-    res.send(path);
+    let path = __dirname+"/frontend/html/page.html";
+    res.sendFile(path);
 })
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;
@@ -17,4 +18,4 @@ const PORT = process.env.PORT || 3000;
 // Start the server
 app.listen(PORT, function(){
     console.log("Server Starting running on http://localhost:"+PORT);
-})
+});

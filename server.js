31cf59838ae325d65ebee373a6e17039427f829a
Deepak Nadiminti <deepaknadiminti12@gmail.com>
2021-04-30 10:48:50 +0530

    added files"

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
  
 const app = express();
  
+app.use(express.static('frontend'));
 app.get("/", function(req, res){
     res.send("Welcome to My Basic Site");
 })
  
 app.get("/resume", function(req, res){
-    res.send("Welcome to My new git Resume page");
+    let path=__dirname+"frontend/html/page.html";
+    res.send(path);
 })
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;

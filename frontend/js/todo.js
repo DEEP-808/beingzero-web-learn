7ba2cd87c1e6ff709cba44c9cd8d6367f687e782
Deepak Nadiminti <deepaknadiminti12@gmail.com>
2021-05-12 23:21:39 +0530

    added todo list

---
+++ frontend/js/todo.js
@@ -0,0 +1,37 @@
+console.log("wazaaaa");
+var i=0;
+var arr=[];
+var input = document.getElementById("task");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   addtolist();
+   //document.getElementById("dem").innerHTML="hey";
+  }
+});
+
+function addtolist(){
+    i++;
+    var taskname=document.getElementById('task').value;
+    var list=document.getElementById('items');
+    var item=document.createElement('li');
+    item.setAttribute('class','incomplete');
+    item.setAttribute('id',i);
+    arr.push(i);
+    item.appendChild(document.createTextNode(taskname));
+    list.appendChild(item);
+    document.getElementById('task').value="";
+    console.log(arr);
+}
+
+function clickme(){
+    console.log("clicked");
+}
+
+function clearall(){
+    console.log(arr);
+    for(let i=0;i<arr.length;i++)
+    {
+        document.getElementById(arr[i]).remove();
+    }
+}
